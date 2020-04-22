dbaa2d512b39cad3a12898404ef1bb49d122a843
nemo_wang@huatek.com <nemo_wang@huatek.com>
2020-04-22 18:06:10 +0800

    测试别名

--- S01_spring/src/com/wangkaijun/MainClass.java
+++ S01_spring/src/com/wangkaijun/MainClass.java
@@ -9,7 +9,11 @@ public class MainClass {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         Object person = context.getBean("person");
+        Object person2 = context.getBean("humn");
+        Object person3 = context.getBean("star");
         System.out.println(ToStringBuilder.reflectionToString(person));
+        System.out.println(ToStringBuilder.reflectionToString(person2));
+        System.out.println(ToStringBuilder.reflectionToString(person3));
     }
 
 }
